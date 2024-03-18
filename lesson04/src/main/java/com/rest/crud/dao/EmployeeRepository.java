b25687750d27785c0d5f59784a748e98becd6187
Dery Cahyadi <dery.cahyadi@anabatic.com>
2024-03-18 20:39:38 +0700

    Lesson 5 Spring Ssecurity

--- lesson04/src/main/java/com/rest/crud/dao/EmployeeRepository.java
+++ lesson04/src/main/java/com/rest/crud/dao/EmployeeRepository.java
@@ -1,12 +1,19 @@
 package com.rest.crud.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.rest.crud.entity.Employee;
 
-@RepositoryRestResource(path = "members")
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	List<Employee> findByFirstName(String firstName);
+	
+	@Query(value = "select t from Employee t where t.email like '%:email%'")
+	List<Employee> findAdjah(String email);
 }
