4416ba9e15509db0f13c9d95a9c01e07490345ed
vionza <vionza@V-Linux>
2024-04-05 01:50:44 +0700

    Initial Lesson 8

---
+++ lesson08/src/test/java/com/spring/mvc/security/Lesson08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mvc.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
